a7bea9c76808244c63e3f856f13328fda24751f1
OllieBouncell <124744251+OllieBouncell@users.noreply.github.com>
2024-04-08 09:52:08 +1200

    Update alienshooter.js
    added color to my start screen and end screen fixed my reset game

--- alienshooter.js
+++ alienshooter.js
@@ -4,8 +4,8 @@ console.log("%c t01_create_sprite", "color: blue;");
 // variables()
 /*******************************************************/
 
-const SCREEN_WIDTH = 400;
-const SCREEN_HEIGHT = 200;;
+const SCREEN_WIDTH = 500;
+const SCREEN_HEIGHT = 300;;
 const OBSTACLE_HEIGHT = 25;
 const OBSTACLE_WIDTH = 25;
 
@@ -20,7 +20,7 @@ var screenSelector = "start";
 /*******************************************************/
 
 function preload() {
-imgPlayer = loadImage('/images/player.png');
+    imgPlayer = loadImage('/images/player.png');
 }
 
 
@@ -31,8 +31,8 @@ function setup() {
     console.log("setup: ");
     
     cnv= new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
-     obstacles = new Group();
-
+    obstacles = new Group();
+// invisble Border around the screen //
     wallLH  = new Sprite(0, SCREEN_HEIGHT/2, 8, SCREEN_HEIGHT , 's');
     wallLH.color = 'lightblue';
     WallLH = noStroke()
@@ -47,83 +47,76 @@ function setup() {
     wallBot.color = 'lightblue';
     WallBot = noStroke()
    
-    
-    
- 
-
- 
- 
- //P5.play website//
+  //  the player and the image for the player //
     player = new Sprite(100, 100, 20, 'd');
     player.color = 'green';
- 
-     player.addImage(imgPlayer);
+
+    player.addImage(imgPlayer);
     imgPlayer.resize(100, 100);
  
-
+ // where you press space to switch through screens //
 document.addEventListener("keydown", 
-        function(event) {
-            if(screenSelector == "start"||screenSelector == "end"){
-                screenSelector = "game"
-                resetGame()
-            }
-        }); 
+    function(event) {
+        if(screenSelector == "start"||screenSelector == "end"){
+            screenSelector = "game"
+            resetGame()
+        }
+    }); 
 
  // Keyboard Movement-Up and Down //
 document.addEventListener("keydown", function(event) {
 
-  if (event.code === 'ArrowLeft') {
-player.vel.x = -3;
+    if (event.code === 'ArrowLeft') {
+    player.vel.x = -3;
     // Set sprite's velocity to the left
 
-  }
+    }
 
- else if 
-(event.code === 'ArrowRight')  player.vel.x = 3;
-else if 
-(event.code === 'ArrowUp') player.vel.y = -3;
-else if 
-(event.code === 'ArrowDown')  player.vel.y = 3; 
+    else if 
+    (event.code === 'ArrowRight')  player.vel.x = 3;
+    else if 
+    (event.code === 'ArrowUp') player.vel.y = -3;
+    else if 
+    (event.code === 'ArrowDown')  player.vel.y = 3; 
 
 
-});
+    });
 
 
 
 document.addEventListener("keyup", function(event) {
 
-  if (event.code === 'ArrowLeft') {
-player.vel.x = 0;
-    // Set sprite's velocity to 0
+    if (event.code === 'ArrowLeft') {
+    player.vel.x = 0; // Set sprite's velocity to 0
+    }
 
-  }
+    else if 
+    (event.code === 'ArrowRight')  player.vel.x = 0;
+    else if 
+    (event.code === 'ArrowUp')  player.vel.y = 0;
+    else if 
+    (event.code === 'ArrowDown')  player.vel.y = 0;
 
- else if 
-(event.code === 'ArrowRight')  player.vel.x = 0;
-else if 
-(event.code === 'ArrowUp')  player.vel.y = 0;
-else if 
-(event.code === 'ArrowDown')  player.vel.y = 0;
+    obstacles.collides(player, playerHitAlien);
 
-obstacles.collides(player, playerHitAlien);
+    bulletGroup = new Group();
+    bulletGroup.collides(obstacle, bulletHitObstacle);
 
-bulletGroup = new Group();
-bulletGroup.collides(obstacle, bulletHitObstacle);
-
-});
+    });
 
 
 
 
-}
+    }
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
- if(screenSelector=="game"){
-        gameScreen();
-if (lives <= 0) {
-    screenSelector = "end";
+ // Selects what function Screen to use when something happenes //
+    if(screenSelector=="game"){
+       gameScreen();
+    if (lives <= 0) {
+        screenSelector = "end";
         }
     }else if(screenSelector=="end"){
         endScreen();
@@ -138,20 +131,22 @@ if (lives <= 0) {
 
 
 
- function keyPressed() {
+function keyPressed() {
+// when a key is pressed the bullet is shot  //
   if (keyCode == 32)
-    bullet = new Sprite( player.x + 50, player.y, 5, 'd');
+        bullet = new Sprite( player.x + 50, player.y, 5, 'd');
     bullet.color = 'yellow';
+    bullet.bounciness = 0;
     bullet.vel.x = 3;
     bulletGroup.add(bullet);
-    
-    bullet.life = 50;// life of how many frames the bullet lasts found on p5.play website
+    bullet.life = 30;// life of how many frames the bullet lasts found on p5.play website
 
 }
 
 
 function newObstacle(){
-    // Code from Geodash
+// how obstacles spawan  //
+    // Code from Geodash  //
     var screenY = random(15,200)
     obstacle = new Sprite(SCREEN_WIDTH, screenY, OBSTACLE_WIDTH, OBSTACLE_HEIGHT, 'k');
     obstacle.color = color("green");
@@ -164,13 +159,14 @@ function newObstacle(){
 
 
 function bulletHitObstacle(bullet, obstacle) {
-    // Remove bullet and obstacle
+    // Remove bullet and obstacle and add score  //
     obstacle.remove();
-     score ++;
+    bullet.remove ();
+    score ++;
 }
 
 function playerHitAlien (_player,alien) {
-
+ // when player hits allien player loses life  //
     lives --;
     console.log("Lives");
  
@@ -183,15 +179,24 @@ function playerHitAlien (_player,alien) {
 
 // Main screen functions
 function startScreen(){
+ // Startscreen function the   //
     allSprites.visible = false;
-    background("white");
-    textSize(32);
+    background("orange");
+    textSize(24);
     fill(255);
     stroke(0);
     strokeWeight(4);
     text("Infinity Assault ", 50, 50);
     textSize(24);
-    text("Press any key to start", 50, 110);
+    text("Press any key to start", 50, 90);
+    textSize(18);
+    text("Rules and How to Play", 50, 130);
+    textSize(14);
+    text("move with the arrow keys", 50, 160);
+    textSize(14);
+    text("shoot the ailens with the space key", 50, 190);
+    textSize(12);
+    text("your goal is to Shoot aliens, score high, avoid losing all three lives or you lose", 50, 230);
 }
 
 
@@ -206,7 +211,7 @@ function gameScreen(){
  player.rotation = 0;
  player.bounciness = 0;
  allSprites.visible = true;
-         for (i = 0; i <lives; i++){
+ for (i = 0; i <lives; i++){
     rect(40 * i, 10, 35, 35);
   }
   fill(0);
@@ -214,7 +219,6 @@ function gameScreen(){
     strokeWeight();
    textSize(32);
     text(score, 320, 40);
-
    if(frameCount> nextSpawn){
         newObstacle();
         nextSpawn = frameCount + random(10,200);
@@ -222,27 +226,29 @@ function gameScreen(){
 }
 
 function endScreen(){
-    background("white");
+    background("orange");
 
     allSprites.visible = false;
-    textSize(32);
+    textSize(24);
     fill(255);
     stroke(0);
     strokeWeight(4);
-    text("You died! Too bad :-(", 50, 50);
+    text("You died. Rest in peace, departed soul.", 50, 50);
     textSize(24);
     text("your score was: "+score, 50, 110);
     textSize(14);
     text("press any key to restart", 50, 150);
-    
-    
-    
-    
+     
 }
 
-function resetGame(){ 
-    gameScreen()
-    obstacles.collides(player, playerHitAlien);
-    bulletGroup.collides(obstacle, bulletHitObstacle);
+function resetGame(){
     score = 0;
-}
+    lives = 3;
+    player.position.x = 100;
+    player.position.y = 100;
+    obstacle.removeSprites();
+    bulletGroup.removeSprites();
+    var spawnDist = 0;
+    var nextSpawn = 0;
+    gameScreen()  
+ }
